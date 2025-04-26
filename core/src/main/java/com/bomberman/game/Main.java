2eea035c6d48fa94bc4635d8e8a4ab610795d697
Remxin <remxin7@gmail.com>
2025-04-26 11:58:06 +0200

    (context): implement global context

--- core/src/main/java/com/bomberman/game/Main.java
+++ core/src/main/java/com/bomberman/game/Main.java
@@ -6,9 +6,11 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.bomberman.screens.GameScreen;
 import com.bomberman.screens.MainScreen;
+import com.bomberman.context.GlobalContext;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
+    public GlobalContext context;
     private SpriteBatch batch;
     private Texture image;
 
