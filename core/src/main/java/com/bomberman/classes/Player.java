714a39606190a055357e792f2da2c7a719b4188d
Antoni Michalczak <antoni0michalczak@gmail.com>
2025-04-26 18:08:01 +0200

    Improve Player class by add directional movement and bomb placement

--- core/src/main/java/com/bomberman/classes/Player.java
+++ core/src/main/java/com/bomberman/classes/Player.java
@@ -1,57 +1,72 @@
 package com.bomberman.classes;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.math.Vector2;
 
 public class Player {
-    private static int MAX_SPEED = 200;
-
     private Vector2 position;
-    private double currentSpeed;
-    private boolean isAlive = true;
+    private static final float SPEED = 200f;
+
     private BombManager bombManager;
+    private boolean isAlive = true;
 
 
-    public Player(Vector2 position, BombManager bombManager) {
-        this.position = new Vector2(position);
+    public Player(Vector2 spawnPosition, BombManager bombManager) {
+        this.position = new Vector2(spawnPosition);
         this.bombManager = bombManager;
     }
 
-    public Vector2 getPosition() {
-        return position;
-    }
+//    public void speedUp(double velocity) {
+//        currentSpeed += velocity;
+//        if (currentSpeed > MAX_SPEED) {
+//            currentSpeed = MAX_SPEED;
+//        }
+//    }
+//
+//    public void speedDown(double velocity) {
+//        currentSpeed -= velocity;
+//        if (currentSpeed < 0) {
+//            currentSpeed = 0;
+//        }
+//    }
+//
+//    public void move(Direction direction) {
+//        switch (direction) {
+//            case UP:
+//                position.y -= currentSpeed;
+//                break;
+//            case DOWN:
+//                position.y += currentSpeed;
+//                break;
+//            case LEFT:
+//                position.x -= currentSpeed;
+//                break;
+//            case RIGHT:
+//                position.x += currentSpeed;
+//                break;
+//        }
+//    }
 
 
-    public void speedUp(double velocity) {
-        currentSpeed += velocity;
-        if (currentSpeed > MAX_SPEED) {
-            currentSpeed = MAX_SPEED;
+    public void update(float delta){
+        if(!isAlive) return;
+
+        int dx = 0;
+        int dy = 0;
+        if      (Gdx.input.isKeyPressed(Input.Keys.UP))    dy =  1;
+        else if (Gdx.input.isKeyPressed(Input.Keys.DOWN))  dy = -1;
+        else if (Gdx.input.isKeyPressed(Input.Keys.LEFT))  dx = -1;
+        else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) dx =  1;
+
+        position.x += dx * SPEED * delta;
+        position.y += dy * SPEED * delta;
+
+        if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE)){
+            placeBomb();
         }
     }
 
-    public void speedDown(double velocity) {
-        currentSpeed -= velocity;
-        if (currentSpeed < 0) {
-            currentSpeed = 0;
-        }
-    }
-
-    public void move(Direction direction) {
-        switch (direction) {
-            case UP:
-                position.y -= currentSpeed;
-                break;
-            case DOWN:
-                position.y += currentSpeed;
-                break;
-            case LEFT:
-                position.x -= currentSpeed;
-                break;
-            case RIGHT:
-                position.x += currentSpeed;
-                break;
-        }
-    }
 
     public void placeBomb() {
         int gridX = (int)(position.x / bombManager.getTileSize());
@@ -59,8 +74,16 @@ public class Player {
         bombManager.placeBomb(gridX, gridY);
     }
 
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
     public void die() {
-        currentSpeed = 0;
         isAlive = false;
     }
+
 }
