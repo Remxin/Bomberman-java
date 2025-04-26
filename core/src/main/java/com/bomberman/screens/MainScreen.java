a19d5ca94cffcf900c00b5d0f8c8529678c97104
Jankess0 <kamilojanik@gmail.com>
2025-04-26 09:02:29 +0200

    background and health button

--- core/src/main/java/com/bomberman/screens/MainScreen.java
+++ core/src/main/java/com/bomberman/screens/MainScreen.java
@@ -6,12 +6,12 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 import javax.swing.*;
 
@@ -20,8 +20,9 @@ public class MainScreen implements Screen {
     private Game game;
     private Texture background;
     private SpriteBatch batch;
-    private Skin skin;
-    private Texture image;
+    private Texture play_button_texture;
+    private Texture health_up_button_texture;
+    private Texture health_down_button_texture;
 
     public MainScreen(Game game) {
         this.game = game;
@@ -31,19 +32,49 @@ public class MainScreen implements Screen {
     public void show() {
         batch = new SpriteBatch();
         stage = new Stage();
-        //image = new Texture("libgdx.png");
-        skin = new Skin(Gdx.files.internal("flat/flat-earth-ui.json"));
+        background = new Texture("MainScreen_background.png");
 
-        TextButton startButton = new TextButton("Start Game", skin);
-        startButton.setPosition(Gdx.graphics.getWidth() / 2.0f - startButton.getWidth() / 2.0f, Gdx.graphics.getHeight() / 2.0f - startButton.getHeight() / 2.0f);
-        startButton.addListener(new ChangeListener() {
+        play_button_texture = new Texture(Gdx.files.internal("play_button.png"));
+        health_up_button_texture = new Texture(Gdx.files.internal("health_up.png"));
+        health_down_button_texture = new Texture(Gdx.files.internal("health_down.png"));
+
+        ImageButton.ImageButtonStyle style_play_button = new ImageButton.ImageButtonStyle();
+        style_play_button.up = new TextureRegionDrawable(new TextureRegion(play_button_texture)); // stan normalny
+        style_play_button.down = new TextureRegionDrawable(new TextureRegion(play_button_texture)); // stan wciśnięty (możesz użyć innej tekstury)
+
+        ImageButton.ImageButtonStyle style_health_button = new ImageButton.ImageButtonStyle();
+        style_health_button.up = new TextureRegionDrawable(new TextureRegion(health_up_button_texture)); // stan domyślny
+        style_health_button.down = new TextureRegionDrawable(new TextureRegion(health_down_button_texture));
+
+        ImageButton play_button = new ImageButton(style_play_button);
+        play_button.setPosition(Gdx.graphics.getWidth() / 2.0f - play_button.getWidth() / 2.0f, Gdx.graphics.getHeight() / 4.5f - play_button.getHeight() / 2.0f);
+
+        ImageButton health_button = new ImageButton(style_health_button);
+        health_button.setPosition(Gdx.graphics.getWidth() / 2.0f -health_button.getWidth() / 2.0f, Gdx.graphics.getHeight() / 2.0f - health_button.getHeight() / 2.0f);
+
+        health_button.getStyle().up = new TextureRegionDrawable(new TextureRegion(health_up_button_texture));
+        health_button.setChecked(false);
+
+        play_button.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent changeEvent, Actor actor) {
                 game.setScreen(new GameScreen(game));
             }
         });
 
-        stage.addActor(startButton);
+        health_button.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                if (health_button.isChecked()) {
+                    health_button.getStyle().up = new TextureRegionDrawable(new TextureRegion(health_down_button_texture));
+                } else {
+                    health_button.getStyle().up = new TextureRegionDrawable(new TextureRegion(health_up_button_texture));
+                }
+            }
+        });
+
+        stage.addActor(play_button);
+        stage.addActor(health_button);
 
         Gdx.input.setInputProcessor(stage);
     }
@@ -52,6 +83,10 @@ public class MainScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        batch.draw(background, 0, 0);
+        batch.end();
 
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         stage.draw();
@@ -65,7 +100,6 @@ public class MainScreen implements Screen {
     @Override
     public void hide() {
         stage.dispose();
-        skin.dispose();
     }
 
     @Override
